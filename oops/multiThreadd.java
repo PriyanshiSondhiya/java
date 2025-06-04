ab1dad5a21b4655acee63664b1d85164fb461012
Priyanshi Sondhiya <priyanshisondhiya8@gmail.com>
2025-06-04 12:49:37 +0530

    day30

--- oops/multiThreadd.java
+++ oops/multiThreadd.java
@@ -28,3 +28,20 @@
 //         t3.start();
 //     }
 // }
+
+
+
+//****2nd method of multithreading */
+class MyRunnable implements Runnable {
+    public void run(){
+        System.out.println(Thread.currentThread().getName()+"hii");
+    }
+}
+
+class multiThreadd{
+    public static void main(String args[]){
+        MyRunnable obj= new MyRunnable();
+        Thread t1= new Thread(obj);
+        t1.start();
+    }
+}
