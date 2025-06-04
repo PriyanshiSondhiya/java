4040b3701c48a558a5e8480ae8c1b41846fa937b
Priyanshi Sondhiya <priyanshisondhiya8@gmail.com>
2025-06-04 13:36:26 +0530

    day 30

---
+++ oops/multiThreadBank.java
@@ -0,0 +1,83 @@
+// class withdrawThread extends Thread{
+//     static int totalBalance=1000;
+//     int amount;
+
+//     withdrawThread(int amount){
+//         this.amount=amount;
+//     }
+
+//     public void run(){
+//         if(totalBalance >=amount){
+//             System.out.println("paisa aapke pass aa jayga kripaya prateeksha karein" + amount);
+
+//             try{
+//                 Thread.sleep(1000);
+//             }catch (InterruptedException e){
+//                 //TODO Auto-generated catch block
+//                 e.printStackTrace();
+//             }
+//             totalBalance-=amount;
+//             System.out.println(totalBalance);
+//         }
+//         else{
+//             System.out.println("Insuffient Balance");
+//         }
+//     }
+// }
+
+
+// public class multiThreadBank {
+//     public static void main(String args[]){
+//     withdrawThread obj= new withdrawThread(800);
+//     withdrawThread obj2= new withdrawThread(500);
+//     obj.start();
+//     obj2.start();
+
+//     }
+// }
+
+
+//****solution */
+class withdrawThread extends Thread{
+    static int totalBalance=1000;
+    int amount;
+
+    withdrawThread(int amount){
+        this.amount=amount;
+    }
+
+    public void run(){
+        if(totalBalance >=amount){
+            System.out.println("paisa aapke pass aa jayga kripaya prateeksha karein" + amount);
+
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                //TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            totalBalance-=amount;
+            System.out.println(totalBalance);
+        }
+        else{
+            System.out.println("Insuffient Balance");
+        }
+    }
+}
+
+
+public class multiThreadBank {
+    public static void main(String args[]){
+    withdrawThread obj= new withdrawThread(800);
+    withdrawThread obj2= new withdrawThread(500);
+    obj.start();
+   
+    try{
+        obj.join();
+    }catch(InterruptedException e){
+        e.printStackTrace();
+    }
+    obj2.start();
+
+    }
+}
--- oops/multiThreadd.java
+++ oops/multiThreadd.java
@@ -1,47 +1,47 @@
-// class FileDownload extends Thread{
-//     String fileName;
+class FileDownload extends Thread{
+    String fileName;
 
-//     public FileDownload(String fileName){
-//         this.fileName=fileName;
-//     }
-
-//     public void run(){
-//         for(int i=1; i<=5; i++){
-//             System.out.println(fileName + " " + "downloading...." + (i*20) + "%");
-//             try{
-//                 Thread.sleep(2000);
-//             }catch(Exception e){
-
-//             }
-//         }
-//     System.out.println(fileName + "done");
-// }
-// }
-
-// class multiThreadd{
-//     public static void main(String args[]){
-//         FileDownload t1= new FileDownload("File A");
-//         FileDownload t2= new FileDownload("File B");
-//         FileDownload t3= new FileDownload("File C");
-//         t1.start();
-//         t2.start();
-//         t3.start();
-//     }
-// }
-
-
-
-//****2nd method of multithreading */
-class MyRunnable implements Runnable {
-    public void run(){
-        System.out.println(Thread.currentThread().getName()+"hii");
+    public FileDownload(String fileName){
+        this.fileName=fileName;
     }
+
+    public void run(){
+        for(int i=1; i<=5; i++){
+            System.out.println(fileName + " " + "downloading...." + (i*20) + "%");
+            try{
+                Thread.sleep(2000);
+            }catch(Exception e){
+
+            }
+        }
+    System.out.println(fileName + "done");
+}
 }
 
 class multiThreadd{
     public static void main(String args[]){
-        MyRunnable obj= new MyRunnable();
-        Thread t1= new Thread(obj);
+        FileDownload t1= new FileDownload("File A");
+        FileDownload t2= new FileDownload("File B");
+        FileDownload t3= new FileDownload("File C");
         t1.start();
+        t2.start();
+        t3.start();
     }
 }
+
+
+
+// //****2nd method of multithreading */
+// class MyRunnable implements Runnable {
+//     public void run(){
+//         System.out.println(Thread.currentThread().getName()+"hii");
+//     }
+// }
+
+// class multiThreadd{
+//     public static void main(String args[]){
+//         MyRunnable obj= new MyRunnable();
+//         Thread t1= new Thread(obj);
+//         t1.start();
+//     }
+// }
