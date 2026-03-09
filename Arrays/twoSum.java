ee82d4d4c00e2616cb45b8d546e88fb6902d851e
Priyanshi Sondhiya <priyanshisondhiya8@gmail.com>
2026-03-09 15:09:20 +0530

    Added array program

---
+++ Arrays/twoSum.java
@@ -0,0 +1,25 @@
+// public class twoSum {
+//   public static void main (String args[]){
+//     int arr[] = {3, 5, 7, 8, 12, 15};
+//     int target = 19;
+//     boolean found = false;
+
+//     for(int i =0; i<arr.length; i++){
+//        for (int j=1+1; j<arr.length; j++){
+//         if(arr[i] + arr[j] == target){
+//          found = true;
+//           break;
+//         }
+//        }
+//     }
+
+//     if(found == true){
+//       System.out.println("Yes");
+//     }else{
+//       System.out.println("False");
+//     }
+
+//   }
+// }
+
+
