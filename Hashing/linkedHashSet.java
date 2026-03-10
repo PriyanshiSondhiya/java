b93dd6445242aff5cdfbed6696b4eaac8038a6b5
Priyanshi Sondhiya <priyanshisondhiya8@gmail.com>
2026-03-10 12:27:46 +0530

    Added hashing distinct elements code

---
+++ Hashing/linkedHashSet.java
@@ -0,0 +1,17 @@
+// import java.util.*;
+
+// //ordered is maintained using dll;
+
+// public class linkedHashSet {
+//   public static void main (String args[]){
+//   LinkedHashSet<String> lhs = new LinkedHashSet<> ();
+//   lhs.add("Delhi");
+//   lhs.add("mumbai");
+//   lhs.add("Noida");
+//   lhs.add("Bengaluru");
+ 
+//   System.out.println(lhs);
+//   lhs.remove("Delhi");
+//   System.out.println(lhs.size());
+//   }
+// }
---
+++ Hashing/treeSet.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class treeSet {
+  public static void main(String args[]){
+  TreeSet<String> ts = new TreeSet<> ();
+  ts.add("Delhi");
+  ts.add("mumbai");
+  ts.add("Noida");
+  ts.add("Bengaluru");
+
+  System.out.println(ts);
+  }
+}
