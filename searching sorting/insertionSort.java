c34467f36e0e23de2a520f7fd5dcf52577e830ea
Priyanshi Sondhiya <priyanshisondhiya8@gmail.com>
2026-03-15 17:26:18 +0530

    added insertion sort program

---
+++ searching sorting/insertionSort.java
@@ -0,0 +1,26 @@
+public class insertionSort {
+  public static void sort(int arr[]){
+
+    
+
+  for(int i=1; i<arr.length; i++){
+    int curr = arr[i];
+    int prev = i-1;
+    //fingin out the correct pos. to insert
+    while(prev >= 0 && arr[prev]>curr){
+      arr[prev + 1] = arr[prev];
+      prev--; 
+    }
+     //insertion
+    arr[prev+1] = curr;
+  }
+
+ for(int i=0; i<arr.length; i++){
+      System.out.print(arr[i] + " ");
+     }
+  }
+  public static void main (String args[]){
+    int arr[] ={5, 4, 3, 2, 1};
+    sort(arr);
+  }
+}
--- searching sorting/selectionSort.java
+++ searching sorting/selectionSort.java
@@ -1,27 +1,61 @@
-public class selectionSort {
-  public static void selectionSort (int nums[]){
+// public class selectionSort {
+//   public static void selectionSort (int nums[]){
 
-    for(int i=0; i<nums.length-1; i++){
-      int max = i;
-      int j=i+1;
-      for( j=i+1; j<nums.length; j++){
-        if(nums[max] > nums[j]){
-          max = j;
+//     for(int i=0; i<nums.length-1; i++){
+//       int max = i;
+//       int j=i+1;
+//       for( j=i+1; j<nums.length; j++){
+//         if(nums[max] < nums[j]){
+//           max = j;
+//         }
+//       }
+
+//       int temp = nums[max];
+//         nums[max] = nums[i];
+//         nums[i] = temp;
+//     }
+
+//     for(int i=0; i<nums.length; i++){
+//       System.out.print(nums[i] + " ");
+//     }
+//   }
+
+//   public static void main (String args[]){
+//     int nums[] ={5, 4, 3, 2, 1};
+//     selectionSort(nums);
+//   }
+// }
+
+
+public class selectionSort{
+  public static void selectionSort(int arr[]){
+
+    for(int i=0; i<arr.length-1; i++){
+      int minPos = i;
+
+      for(int j=i+1; j<arr.length; j++){
+        if(arr[minPos] > arr[j]){
+          minPos = j;
         }
       }
 
-      int temp = nums[max];
-        nums[max] = nums[i];
-        nums[i] = temp;
-    }
+      //swap
+      int temp = arr[minPos];
+      arr[minPos] = arr[i];
+      arr[i] = temp;
 
-    for(int i=0; i<nums.length; i++){
-      System.out.print(nums[i] + " ");
+      
     }
+    
+    for(int i=0; i<arr.length; i++){
+      System.out.print(arr[i] + " ");
+     }
+    
   }
 
-  public static void main (String args[]){
-    int nums[] ={5, 4, 3, 2, 1};
-    selectionSort(nums);
+  public static void main(String args[]){
+    int arr[] ={5, 4, 3, 2, 1};
+
+    selectionSort(arr);
   }
 }
