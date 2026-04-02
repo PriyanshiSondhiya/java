617599258c62238a303305ba5bf553939ee49dee
Priyanshi Sondhiya <priyanshisondhiya8@gmail.com>
2026-04-02 16:04:58 +0530

    added recursion program

---
+++ recursion/Main.java
@@ -0,0 +1,45 @@
+ import java.util.*;
+
+public class Main {
+
+    private static void findCombination(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for (int i = ind; i < arr.length; i++) {
+
+            // skip duplicates
+            if (i > ind && arr[i] == arr[i - 1]) continue;
+
+            // stop if element > target
+            if (arr[i] > target) break;
+
+            ds.add(arr[i]);
+
+            findCombination(i + 1, arr, target - arr[i], ans, ds);
+
+            ds.remove(ds.size() - 1); // backtrack
+        }
+    }
+
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
+
+        findCombination(0, candidates, target, ans, new ArrayList<>());
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] candidates = {1, 1, 2, 5, 6, 7, 10};
+        int target = 8;
+
+        List<List<Integer>> result = combinationSum2(candidates, target);
+
+        System.out.println(result);
+    }
+} 
+  
+
