583257ca4347b6c214de8a436601776e045c72c8
Priyanshi Sondhiya <priyanshisondhiya8@gmail.com>
2026-04-13 21:20:52 +0530

    added BS search insert position program

---
+++ binarySearch/searchInsertPos.java
@@ -0,0 +1,29 @@
+public class searchInsertPos {
+  
+    public static int searchInsert(int[] nums, int target) {
+        int st =0 , end = nums.length-1;
+        int mid = 0;
+        while(st <= end){
+             mid = st + (end - st ) /2;
+            if(nums[mid] == target){
+                return mid;
+            }else if (nums[mid] > target ){
+                   end = mid -1;
+            }else{
+                  st = mid + 1;
+            }
+        }
+
+        
+            return st;
+        
+        
+        
+    }
+
+    public static void main (String args[]){
+      int nums[] = {1,3,5,6};
+      System.out.println(searchInsert(nums, 2));
+    }
+}
+
