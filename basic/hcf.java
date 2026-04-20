d5b834517fd4114a43ad06fff4173c014cf9b278
Priyanshi Sondhiya <priyanshisondhiya8@gmail.com>
2026-04-20 10:58:48 +0530

    added lcm program

---
+++ basic/hcf.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class hcf {
+  public static int print1 (int a, int b){
+    int gcd = 1;
+    for(int i = 1; i<=Math.min(a, b); i++){
+      if( a% i == 0 && b % i == 0){
+        gcd = i;
+      }
+    }
+    return gcd;
+  }
+
+  public static int print2 (int a, int b){
+  int gcd = 1;
+    for(int i = Math.min(a, b); i>=1; i--){
+      if(a% i == 0 && b%i ==0){
+        gcd = i;
+        break;
+      }
+    }
+    return gcd;
+  }
+
+  public static int print3(int a, int b){
+    while( a> 0 && b > 0){
+      if( a> b) a = a% b; else b = b%a;
+    }
+    if(a == 0)return b;
+    else return a;
+  }
+
+  public static void main (String args[]){
+  System.out.println(print1(9, 12));
+  System.out.println(print2(9, 12));
+  System.out.println(print3(9, 12));
+  }
+}
