86623729f01c1197ab3da864ccaa4494a5330a66
Priyanshi Sondhiya <priyanshisondhiya8@gmail.com>
2026-05-02 23:12:50 +0530

    any base addition

--- basic/anyBaseAddition.java
+++ basic/anyBaseAddition.java
@@ -6,11 +6,13 @@ public class anyBaseAddition {
     while(n1>0 || n2>0 || c>0){
       int d1 = n1%10;
       int d2 = n2%10;
+      n1 = n1/10;
+      n2 = n2/10;
       int d = d1 + d2 + c;
       c = d / 8;
       d = d% 8;
 
-      ans *= d*pow;
+      ans += d*pow;
       pow *= 10;
     }
     System.out.println(ans);
