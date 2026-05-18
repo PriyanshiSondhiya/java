b504839503953b4fecba45cbab9ced4b723327ca
Priyanshi Sondhiya <priyanshisondhiya8@gmail.com>
2026-05-18 22:08:16 +0530

    inverse triangle pattern

--- codeforces/worms.java
+++ codeforces/worms.java
@@ -1,42 +1,50 @@
 import java.util.*;
 
 public class worms {
-  public static void main (String args[]){
-    Scanner sc = new Scanner(System.in);
 
-    int n = sc.nextInt();
-    int pile[] = new int[n+1];
-    for(int i=1; i<=n; i++){
-      pile[i] = sc.nextInt();
-    }
+    public static void main(String args[]) {
 
-    int m = sc.nextInt();
-    int juicyWorm[] = new int[m+1];
-    for(int i=1; i<=m; i++){
-      juicyWorm[i] = sc.nextInt();
-    }
+        Scanner sc = new Scanner(System.in);
 
-    int prefix[] = new int[n+1];
-     
-    prefix[1] = pile[1];
-    for(int i=2; i<=n; i++){
-       prefix[i] = prefix[i-1] + pile[i];
-    }
+        int n = sc.nextInt();
 
-    
-     
-    
-      for(int i=1; i<=m; i++){
-        int q = juicyWorm[i];
-        for(int j=1; i<=n; j++){
-          if(prefix[j] >= q ){
-          System.out.println(j);
-          break;
+        int pile[] = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            pile[i] = sc.nextInt();
         }
-          
+
+        int prefix[] = new int[n + 1];
+
+        prefix[1] = pile[1];
+
+        for (int i = 2; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + pile[i];
         }
-      }
-       
-   
-  }
+
+        int m = sc.nextInt();
+
+        for (int i = 1; i <= m; i++) {
+
+            int q = sc.nextInt();
+
+            int low = 1;
+            int high = n;
+            int ans = -1;
+
+            while (low <= high) {
+
+                int mid = (low + high) / 2;
+
+                if (prefix[mid] >= q) {
+                    ans = mid;
+                    high = mid - 1; // left side me aur chota answer dhundo
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+            System.out.println(ans);
+        }
+    }
 }
