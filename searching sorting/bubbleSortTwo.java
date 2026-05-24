629b3e4441502fb89a9e23e295418a1026934f03
Priyanshi Sondhiya <priyanshisondhiya8@gmail.com>
2026-05-24 22:09:24 +0530

    bubble sort

---
+++ searching sorting/bubbleSortTwo.java
@@ -0,0 +1,27 @@
+public class bubbleSortTwo {
+    public static void main(String[] args) {
+
+        int[] arr = {4, 2, 7, 1, 9};
+
+        // Bubble Sort
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
